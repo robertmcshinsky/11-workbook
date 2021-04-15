33cbe223a4f3e299e10cbee69c3546d894ec340c
robertmcshinsky <robert.mcshinsky@gmail.com>
2021-04-15 14:25:50 -0600

    get id express thingy

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const { animals } = require("./data/animals");
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 
 function filterByQuery(query, animalsArray) {
   let personalityTraitsArray = [];
@@ -57,6 +57,19 @@ app.get("/api/animals", (req, res) => {
   res.json(results);
 });
 
+function findById(id, animalsArray) {
+  const result = animalsArray.filter((animal) => animal.id === id)[0];
+  return result;
+}
+
+app.get("/api/animals/:id", (req, res) => {
+  const result = findById(req.params.id, animals);
+  if (result) {
+    res.json(result);
+  } else {
+    res.send(404);
+  }
+});
 // -------------------------------------------------
 app.listen(PORT, () => {
   console.log("API server now on port 3000");
